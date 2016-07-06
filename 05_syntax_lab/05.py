bdd71d1bb87133e3aa0a474e60c6c578ce72eb0b
shlomitTal <shlomit.yona@verint.com>
2016-07-06 12:30:59 +0300

    Solution for: sol-python-verint.syntax-lab/05.py

--- 05_syntax_lab/05.py
+++ 05_syntax_lab/05.py
@@ -1,5 +1,8 @@
-"""
-Write a program that randomizes integers in a loop
-until it finds a number that is divisable by: 7, 13 and 15
-"""
+#commited by Shlomit
 
+from random import randint
+
+while True:
+    num = randint(1, 1000000)
+    if (num % 7 == 0 and num % 13 == 0 and num % 15 == 0): break
+print "the num:", num, "is divied by 7, 13 and 15"
