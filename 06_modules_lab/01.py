b52056aead0559b9c20e03293544177188397e35
shlomitTal <shlomit.yona@verint.com>
2016-07-07 15:20:32 +0300

    Solution for: sol-python-verint.modules-lab/01.py

--- 06_modules_lab/01.py
+++ 06_modules_lab/01.py
@@ -1,4 +1,19 @@
-""" Write a program that takes a count
-from sys.argv import and prints "Hello Python"
-count times
-"""
+#Commited by Shlomit
+import sys
+if(len(sys.argv)) < 1:
+    print "error - you didn't add any arguments!"
+    sys.exit
+
+loopCount = sys.argv[1]
+
+if(loopCount[0] == "-"):
+      print "error - you enter a negetive number!"
+if(not loopCount.isdigit()):
+     print "error - you enter!"
+     sys.exit
+
+i = 0
+while i < int(loopCount):
+    print "Hello Python"
+    i+=1
+
