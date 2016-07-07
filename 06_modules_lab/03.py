9878402bcb097828d8f1e5d05e31752cdbc89acd
shlomitTal <shlomit.yona@verint.com>
2016-07-07 17:29:54 +0300

    Solution for: sol-python-verint.modules-lab/03.py

--- 06_modules_lab/03.py
+++ 06_modules_lab/03.py
@@ -1,11 +1,30 @@
-""" Write a program that searches current working directory
-for files larger than 1MB. Every time you find such a file print
-its name to the user.
+#Commited by Shlomit
+import sys
+import os
 
-- When the program finds a large file. It should ask the user
-  a message asking if she wants to delete it, and delete the
-  file if requested
+if(len(sys.argv)) < 3:
+    print "error - you didn't add  arguments!"
+    sys.exit
 
-- Take threshold and path as command line arguments
-"""
+path = sys.argv[1]
+if not os.path.isdir(path):
+    print "Invalid folder"
+    sys.exit() 
 
+
+size = sys.argv[2]
+if(not size.isdigit()):
+    print "error - second arg is not a number"
+    sys.exit
+
+intsize= int(size) * 1024 *1024
+for root, dirs,files in os.walk(path):
+    for name in files:
+        fullFilePathName = os.path.join (root,name)
+        statinfo = os.stat(fullFilePathName)
+        if(statinfo.st_size > intsize):
+            print "file:"+ name +"is  bigger then:%", sys.argv[2], "mega"
+            shouldRemove = raw_input("do you want to remove the file:"+ name +"? please enter Yes Or No\n")
+            if(shouldRemove[0].lower() == 'y'):
+                os.remove(fullFilePathName)
+            
