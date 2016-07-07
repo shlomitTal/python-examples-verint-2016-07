985db92333c326e966e436126673c6434bcacbd0
shlomitTal <shlomit.yona@verint.com>
2016-07-07 15:50:07 +0300

    Solution for: sol-python-verint.modules-lab/02.py

--- 06_modules_lab/02.py
+++ 06_modules_lab/02.py
@@ -1,4 +1,11 @@
-""" Write a program that reads 2 numbers from sys.argv
-and prints their sum.
-"""
+#Commited by Shlomit
+import sys
+if(len(sys.argv)) < 3:
+    print "error - you didn't add any arguments!"
+    sys.exit
+
+arg1 = int(sys.argv[1])
+arg2 = int(sys.argv[2])
+
+print "the sum of :",(arg1, arg2), "is:", arg1 + arg2
 
