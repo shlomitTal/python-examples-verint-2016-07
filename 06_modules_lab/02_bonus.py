f905f2062e25b9ffefb3ab9dd99f28c49b910f72
shlomitTal <shlomit.yona@verint.com>
2016-07-07 15:50:11 +0300

    Solution for: sol-python-verint.modules-lab/02_bonus.py

--- 06_modules_lab/02_bonus.py
+++ 06_modules_lab/02_bonus.py
@@ -1,10 +1,14 @@
-""" Write a program that reads 2 numbers from sys.argv
-and prints their sum.
-Bonus: Print error messages for invalid inputs.
+#Commited by Shlomit
+import sys
+if(len(sys.argv)) < 3:
+    print "error - you didn't add any arguments!"
+    sys.exit
 
-To print error messages we'll have to use a concept not yet learned in the
-course, and which will only be presented later: Exceptions.
-We'll tap into python's error handling and change its default
-error message to something more meaningful.
-"""
+arg1 = (sys.argv[1]).lstrip("-+")
+arg2 = (sys.argv[2]).lstrip("-+")
 
+if(not arg1.isdigit() or not arg2.isdigit()):
+     print "error - you dont enter a number!"
+     sys.exit
+
+print "the sum of :",(arg1, arg2), "is:", int(arg1) + int(arg2)
