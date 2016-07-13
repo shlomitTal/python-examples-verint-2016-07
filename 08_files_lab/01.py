9c8703e444c7d5d5375a5763e9b9026e196b9663
shlomitTal <shlomit.yona@verint.com>
2016-07-13 16:48:58 +0300

    Solution for: sol-python-verint.files-lab/01.py

--- 08_files_lab/01.py
+++ 08_files_lab/01.py
@@ -1,6 +1,16 @@
-"""
-Write a program that takes 2 file names
-and appends the second file's contents to
-the end of the first one
-"""
+#Commited by Shlomit
+#D:\tmp\t1.txt D:\tmp\t2.txt
 
+import sys
+if len(sys.argv) < 3:
+    print "error -not enugh args"
+    sys.exit
+
+file1 = sys.argv[1]
+file2 = sys.argv[2]
+
+
+with open(file1, "r") as fr:
+    with open (file2, "a") as fw:
+        for line in fr:
+            fw.write(line);
