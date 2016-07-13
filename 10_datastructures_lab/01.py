a8846aa49d1f50da1870ad83414fc4a7d4d7758d
shlomitTal <shlomit.yona@verint.com>
2016-07-13 17:31:47 +0300

    Solution for: sol-python-verint.datastructures-lab/01.py

--- 10_datastructures_lab/01.py
+++ 10_datastructures_lab/01.py
@@ -1,10 +1,23 @@
-"""
-Write a program that takes two strings
-from the user and checks if they represent
-a valid user name.
-Valid users and passwords:
-    apple => red
-    lettuce => green
-    lemon => yellow
-    orange => orange
-"""
+#commited by shlomit
+import sys
+
+if len(sys.argv) < 2:
+    print "error - you didn't add any arguments!"
+    sys.exit
+
+hashKey={}
+file = sys.argv[1]
+with open(file, "r") as f:
+    for line in f:
+        fields = line.split("=")
+        if len(fields) != 2:
+            continue
+        hashKey[fields[0]] = fields[1]
+
+print "please write the wanted computer names"
+
+for row in raw_input().split():
+    if row in hashKey:
+        print "the ip of computer name: "+ row+ " is: "+ hashKey[row]
+    else:
+         print "error - the computer name: "+ row + " is not exist in the file"
