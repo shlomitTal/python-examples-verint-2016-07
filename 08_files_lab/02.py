89e8983c8a11a3c5469090e8597741fb7437887b
shlomitTal <shlomit.yona@verint.com>
2016-07-13 16:48:59 +0300

    Solution for: sol-python-verint.files-lab/02.py

--- 08_files_lab/02.py
+++ 08_files_lab/02.py
@@ -1,7 +1,17 @@
-"""
-Write a program that takes 3 names. The first two
-are existing file names and the last is a new file name
-Program should write the lines from the first two 
-files interwinded into the output file
-"""
+#Commited by Shlomit
+import sys, itertools
+
+if len(sys.argv) < 4:
+    print "error - you didn't add any arguments!"
+    sys.exit
+
+
+with open(sys.argv[1], "r") as fr1:
+    with open(sys.argv[2], "r") as fr2:
+        with open(sys.argv[3], "w") as fw3:
+            for rows in itertools.izip_longest(fr1, fr2):
+                for i in rows:
+                    if i is not None:
+                     fw3.write(i)
+
 
