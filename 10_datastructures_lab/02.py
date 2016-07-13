28426c2148d00c35daa14b4a6b8a5a29aeff1828
shlomitTal <shlomit.yona@verint.com>
2016-07-13 22:01:02 +0300

    Solution for: sol-python-verint.datastructures-lab/02.py

--- 10_datastructures_lab/02.py
+++ 10_datastructures_lab/02.py
@@ -1,5 +1,19 @@
-"""
-Write a program that prints only unique values
-from sys.argv (without program name)
-"""
+#commited by Shlomit
+import sys
+from collections import defaultdict
 
+if len(sys.argv) < 2:
+    print "error - you didn't add any arguments!"
+    sys.exit
+file = sys.argv[1]
+hashmap=defaultdict(str)
+
+with open(file, "r") as f:
+    for row in f:
+       rowVal = row[:-1] if row.endswith("\n") else row            
+       sortedRow = "".join([i for i in sorted(rowVal)])
+       hashmap[sortedRow] += rowVal + " " 
+    
+for row in hashmap:
+    print (hashmap[row])[:-1]
+   
