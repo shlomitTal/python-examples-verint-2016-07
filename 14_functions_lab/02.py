1b8a4f55dcbfece89da83a073335d10652e259ae
shlomitTal <shlomit.yona@verint.com>
2016-07-14 16:33:09 +0300

    Solution for: sol-python-verint.functions-lab/02.py

--- 14_functions_lab/02.py
+++ 14_functions_lab/02.py
@@ -1,7 +1,11 @@
-"""
-Write a function that takes two arguments: 
-    A string
-    And a number
-If wrong types were passed in, raise an exception
-"""
+#commited by Shlomit
 
+import numbers
+def validateTypes(num, st):
+    if not isinstance(num, numbers.Number):
+        raise Exception("Invalid argument type - num arg is not a number")
+    if not isinstance(st, basestring):
+        raise Exception("Invalid argument type - st arg is not a string")
+    print num , st
+
+#validateTypes(5, "ffgd")
