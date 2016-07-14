22045122795622f1342e6f41317a3e5983d91dde
shlomitTal <shlomit.yona@verint.com>
2016-07-14 17:07:20 +0300

    Solution for: sol-python-verint.functions-lab/04.py

--- 14_functions_lab/04.py
+++ 14_functions_lab/04.py
@@ -1,6 +1,14 @@
-"""
-Write a function that takes minlen and
-a list of words, and returns only the words
-longer than minlen
-"""
+#commited by Shlomit
+import numbers
+list=[]
+def longer_than(num, *args):
+    if not isinstance(num, numbers.Integral):
+        raise Exception("not send a number")
+    for i in args:
+        if not isinstance(i, basestring):
+            continue
+        if len(i) > num:
+            list.append(i)
+    return list
 
+#print longer_than(5, "hhh", "gggggggg", "h", "ttttttttt")
