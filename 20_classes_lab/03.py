73b6040d20bcf0ef21870f0f82dee73b6a9b6a1e
shlomitTal <shlomit.yona@verint.com>
2016-07-23 23:00:43 +0300

    Solution for: sol-python-verint.class-syntax-lab/03.py

--- 20_classes_lab/03.py
+++ 20_classes_lab/03.py
@@ -1,13 +1,55 @@
-"""
-Following code assumes a class named Widget that depends on
-other widgets
-Widget ctor takes a name, and function 
-add_dependency(depends)
-marks widget as depends on all widgets in "depends" list
+#commited by Shlomit
+class Widget:
+    def __init__(self, st):
+        self.st = st
+        self.myObjlist=[]
+        self.myStrlist=[]
+        self.doneBuild=0
+        self.depSet = set()
 
-When building a widget, you need to first build all the widgets it
-depends on.
+    def add_dependency(self,*_list):
+        for obj in _list:
+            self.myObjlist.append(Widget(obj))
+            st1= obj.st
+            self.myStrlist.append(st1)
 
-Implement widget so the following code works
-"""
+    def PreBuild(self, obj):
+        list=[]
+        if not obj.doneBuild:
+            obj.doneBuild = 1
+            st1 = obj.st.st
+            self.depSet.add(st1)
+            for a in obj.st.myObjlist:
+                list.append(self.PreBuild(a))
+               # 
+           
+        return list
 
+    
+    def build(self):
+        uniqe = set()
+        lis=[]
+        for a in self.myObjlist:
+            lis.append(self.PreBuild(a))
+        return self.depSet
+
+    
+            
+
+
+
+#luka =Widget("luka")
+#hansolo =Widget("han solo")
+#liea=Widget("leia")
+#yoda=Widget("yoda")
+#padam=Widget("padam amili")
+#anakin=Widget("anakin sysler")
+#obi=Widget("obi-wan")
+#darth=Widget("darth vad")
+#_all = Widget("All")
+#luka.add_dependency(hansolo, liea,yoda)
+#liea.add_dependency(padam,anakin)
+#darth.add_dependency(anakin)
+#_all.add_dependency(luka,hansolo,liea,yoda,padam,anakin,obi,darth)
+#a = _all.build()
+#print a
