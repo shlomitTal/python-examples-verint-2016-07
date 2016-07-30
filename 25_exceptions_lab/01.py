cac7b2eeeed7db7a743578b6ee7e50eaf03ae5e2
shlomitTal <shlomit.yona@verint.com>
2016-07-30 23:20:38 +0300

    Solution for: sol-python-verint.exceptions-lab/01.py

--- 25_exceptions_lab/01.py
+++ 25_exceptions_lab/01.py
@@ -1,7 +1,22 @@
-"""
-Write a python program that takes numbers in a loop
-and for each number prints its square root
-If value is negative or not a number show 
-a warning and keep reading values
-"""
+#commited by Shlomit
+
+import numbers
+import sys
+import math
+def sqrt(n):
+    if not isinstance(n, numbers.Number):
+        raise ValueError("Invalid argument type - num arg is not a number")
+    elif n < 0:
+        raise ValueError ("expected parameter > 0, got: %d"% n)
+
+    return math.sqrt(n)
+
+
+while True:
+  #  try:
+        num = int(raw_input())
+        sqrt(num)
+  #  except ValueError as e:
+  #     print "failed to calc sqrt. error was %s"% e.message
+         
 
